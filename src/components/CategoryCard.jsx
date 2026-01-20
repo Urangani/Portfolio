914f62cde0421b6cbd763c843060c9dd9268794b
Urangani <www.uranganidc@gmail.com>
2026-01-20 17:32:42 +0200

    Update CategoryCard styles: add borders for improved visual separation

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -3,13 +3,13 @@ import React from "react";
 // Reusable Card Component
 const CategoryCard = ({ title, items }) => {
   return (
-    <div className="bg-zinc-800 shadow-md rounded-lg p-6 w-60">
+    <div className=" shadow-md border border-zinc-800 rounded-lg p-6 w-60">
       <h2 className="text-xl font-semibold text-white mb-4">{title}</h2>
       <ul className="flex flex-wrap">
         {items.map((item, index) => (
           <li
             key={index}
-            className="bg-black text-white text-center text-sm rounded-lg m-2 p-2"
+            className="bg-black border border-zinc-800 text-white text-center text-sm rounded-lg m-2 p-2"
           >
             {item}
           </li>
