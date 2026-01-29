54f26950ef3019918c75a33f3dba09d261ae61ad
Urangani <www.uranganidc@gmail.com>
2026-01-29 19:09:20 +0200

    added responsive design and react icons

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,10 +5,10 @@ import skills from "../data/skills.json"; // adjust path as needed
 
 export default function Skills() {
   return (
-    <section className="py-16 border-t border-zinc-800">
+    <section className="flex flex-col py-16 border-t border-zinc-800">
       <h2 className="text-2xl font-semibold mb-10">Skills</h2>
 
-      <div className="flex flex-nowrap gap-4 overflow-x-auto md:flex-wrap scrollbar-hide">
+      <div className="flex justify-center flex-wrap gap-4 scrollbar-hide">
         {skills.categories.map((cat) => (
           <CategoryCard key={cat.id} title={cat.title} items={cat.items} />
         ))}
