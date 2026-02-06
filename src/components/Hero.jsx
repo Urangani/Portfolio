d13e503d703da9f0c4efb0ec690c507da089b316
Urangani <www.uranganidc@gmail.com>
2026-02-06 14:47:03 +0200

    create readable paragraph

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,10 +17,16 @@ export default function Hero() {
     <p className="mt-4 text-zinc-400">
       I am a passionate software developer and engineer with a
       Bachelor’s degree in Computer Science from the University of
-      Venda. I build practical software systems with a focus on clean
+      Venda.
+    </p>
+
+      <p className="mt-4 text-zinc-400"> I build practical software systems with a focus on clean
       architecture and scalable solutions. My work is driven by a love
       for solving everyday problems and creating tools that simplify
-      life’s mundane tasks. Beyond code, I am a poet, gamer, and music
+      life’s mundane tasks.
+      </p> 
+      
+      <p  className="mt-4 text-zinc-400"> Beyond code, I am a poet, gamer, and music
       lover—bringing creativity, curiosity, and rhythm into everything I
       do. With a strong dedication to software engineering and
       development, I strive to merge technical excellence with artistic
